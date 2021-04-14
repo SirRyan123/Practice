ebc6dd62e7455a1be821d5ed162fa9d64264d2bd
Ryan Demboski <ryandemboski7@gmail.com>
2021-04-14 10:58:09 -0700

    Create thing.py

---
+++ Practice/thing.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, color, mileage):
+        self.color = color
+        self.mileage = mileage
+
+    def __repr__(self):
+        return '__repr__ for Car'
+
+    def __str__(self):
+        return '__str__ for Car'
